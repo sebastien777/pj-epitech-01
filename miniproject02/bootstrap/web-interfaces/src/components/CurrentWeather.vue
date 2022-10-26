fe5484588aebcf1df351410c56ea22978eb9a27e
sebastien <baravalle.sebastien@gmail.com>
2022-10-26 12:16:12 +0200

    bootstrap miniproject

---
+++ miniproject02/bootstrap/web-interfaces/src/components/CurrentWeather.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="weather">
+    <div class="row">
+      <slot name="c"></slot>
+    </div>
+    <div class="row">
+      <slot name="t"></slot>
+    </div>
+    <div class="row">
+      <slot name="d"></slot>
+    </div>
+  </div>
+</template>
+<style scoped>
+div.weather {
+  margin: 5px;
+}
+div.row {
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 2px;
+}
+</style>
